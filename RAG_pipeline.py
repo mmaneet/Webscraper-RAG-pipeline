70f448dddb5eb8aa81b3801d26ba357ef19b8f41
Maneet Mehta <68968417+mmaneet@users.noreply.github.com>
2024-05-06 15:43:59 -0400

    Add files via upload

---
+++ RAG_pipeline.py
@@ -0,0 +1,28 @@
+from haystack.telemetry import tutorial_running
+from datasets import load_dataset
+from haystack import Document
+from haystack.components.embedders import SentenceTransformersDocumentEmbedder
+from haystack.document_stores.in_memory import InMemoryDocumentStore
+from haystack.components.embedders import SentenceTransformersTextEmbedder
+from haystack.components.retrievers.in_memory import InMemoryEmbeddingRetriever
+from haystack.components.builders import PromptBuilder
+
+tutorial_running(27)
+
+
+document_store = InMemoryDocumentStore()
+
+dataset = load_dataset("PDF_texts", split="train")
+docs = [Document(content=doc["text"]) for doc in dataset]
+
+doc_embedder = SentenceTransformersDocumentEmbedder(model="sentence-transformers/all-MiniLM-L6-v2")
+doc_embedder.warm_up()
+
+docs_with_embeddings = doc_embedder.run(docs)
+document_store.write_documents(docs_with_embeddings["documents"])
+
+text_embedder = SentenceTransformersTextEmbedder(model="sentence-transformers/all-MiniLM-L6-v2")
+
+retriever = InMemoryEmbeddingRetriever(document_store)
+
+
