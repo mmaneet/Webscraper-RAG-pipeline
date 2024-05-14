62f1f2202dadad72c19bd3f98934c0e13e59392b
Maneet Mehta <68968417+mmaneet@users.noreply.github.com>
2024-05-14 14:34:11 -0400

    Add files via upload

--- RAG_pipeline.py
+++ RAG_pipeline.py
@@ -1,3 +1,4 @@
+from haystack.document_stores.types import DuplicatePolicy
 from haystack.telemetry import tutorial_running
 from datasets import load_dataset
 from haystack import Document
@@ -6,6 +7,10 @@ from haystack.document_stores.in_memory import InMemoryDocumentStore
 from haystack.components.embedders import SentenceTransformersTextEmbedder
 from haystack.components.retrievers.in_memory import InMemoryEmbeddingRetriever
 from haystack.components.builders import PromptBuilder
+import os
+from getpass import getpass
+from haystack.components.generators import OpenAIGenerator
+from haystack import Pipeline
 
 tutorial_running(27)
 
@@ -19,10 +24,50 @@ doc_embedder = SentenceTransformersDocumentEmbedder(model="sentence-transformers
 doc_embedder.warm_up()
 
 docs_with_embeddings = doc_embedder.run(docs)
-document_store.write_documents(docs_with_embeddings["documents"])
+document_store.write_documents(docs_with_embeddings["documents"], policy=DuplicatePolicy.OVERWRITE)
 
 text_embedder = SentenceTransformersTextEmbedder(model="sentence-transformers/all-MiniLM-L6-v2")
 
 retriever = InMemoryEmbeddingRetriever(document_store)
+
+template = """
+Given the following information, answer the question.
+Use minimal background knowledge and focus on the context provided.
+
+Context:
+{% for document in documents %}
+    {{ document.content }}
+{% endfor %}
+
+Question: {{question}}
+Answer:
+"""
+
+prompt_builder = PromptBuilder(template=template)
+
+if "OPENAI_API_KEY" not in os.environ:
+    os.environ["OPENAI_API_KEY"] = getpass("Enter OpenAI API key:")
+generator = OpenAIGenerator(model="gpt-3.5-turbo-0125")
+
+
+
+basic_rag_pipeline = Pipeline()
+# Add components to your pipeline
+basic_rag_pipeline.add_component("text_embedder", text_embedder)
+basic_rag_pipeline.add_component("retriever", retriever)
+basic_rag_pipeline.add_component("prompt_builder", prompt_builder)
+basic_rag_pipeline.add_component("llm", generator)
+
+# Now, connect the components to each other
+basic_rag_pipeline.connect("text_embedder.embedding", "retriever.query_embedding")
+basic_rag_pipeline.connect("retriever", "prompt_builder.documents")
+basic_rag_pipeline.connect("prompt_builder", "llm")
+
+question = "What are three techniques I can use to make effective AI art?"
+
+response = basic_rag_pipeline.run({"text_embedder": {"text": question}, "prompt_builder": {"question": question}})
+
+print(response)
+print(response["llm"]["replies"][0])
 
 
